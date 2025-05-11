50d6feafe6b7460c94eaa3eb0ea666695e9d9b9c
Vipin Yadav <imvpn22@gmail.com>
2025-05-11 07:43:08 +0000

    padding adjust

--- data/profile.js
+++ data/profile.js
@@ -4,7 +4,7 @@ const profileData = {
   sub_title: "Senior Software Engineer",
   logoURL: "assets/images/dp.jpg",
   about: {
-    intro: `Senior Software Engineer with 6 years of frontend development experience. Expert in building high-performance UI and seamless UX. Experienced in leading projects, migrating legacy systems, and creating reusable components. Skilled in A/B testing, cross-platform compatibility, and responsive design. Effective mentor and collaborator dedicated to continuous learning and innovation.`,
+    intro: `Senior Software Engineer with almost 7 years of frontend development experience. Expert in building high-performance UI and seamless UX. Experienced in leading projects, migrating legacy systems, and creating reusable components. Skilled in A/B testing, cross-platform compatibility, and responsive design. Effective mentor and collaborator dedicated to continuous learning and innovation.`,
     contact: {
       email: "imvpn22@gmail.com",
       phone: "+91-9919716378",
@@ -152,6 +152,12 @@ const profileData = {
       duration: "2011 - 2013",
       std: "Class XI-XII (PCM)",
       score: "89.80%",
+    },
+    {
+      alma: "Jwala Devi SVMIC, Prayagraj",
+      duration: "2010 - 2011",
+      std: "Class X (PCM)",
+      score: "73.30%",
     },
   ],
   certifications: [
